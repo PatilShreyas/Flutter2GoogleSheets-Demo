3a2b18aeed5973340373282f4bc8b44078afd605
patilshreyas <shreyaspatilg@gmail.com>
2020-07-04 12:03:09 +0530

    Add screen for responses list

---
+++ lib/feedback_list.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import 'controller/form_controller.dart';
+import 'model/form.dart';
+
+class FeedbackListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Feedback Responses',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FeedbackListPage(title: "Hello"));
+  }
+}
+
+class FeedbackListPage extends StatefulWidget {
+  FeedbackListPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _FeedbackListPageState createState() => _FeedbackListPageState();
+}
+
+class _FeedbackListPageState extends State<FeedbackListPage> {
+  List<FeedbackForm> feedbackItems = List<FeedbackForm>();
+
+  // Method to Submit Feedback and save it in Google Sheets
+
+  @override
+  void initState() {
+    super.initState();
+
+    FormController().getFeedbackList().then((feedbackItems) {
+      setState(() {
+        this.feedbackItems = feedbackItems;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Responses'),
+      ),
+      body: ListView.builder(
+        itemCount: feedbackItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Row(
+              children: <Widget>[
+                Icon(Icons.person),
+                Expanded(
+                  child: Text(
+                      "${feedbackItems[index].name} (${feedbackItems[index].email})"),
+                )
+              ],
+            ),
+            subtitle: Row(
+              children: <Widget>[
+                Icon(Icons.message),
+                Expanded(
+                  child: Text(feedbackItems[index].feedback),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
